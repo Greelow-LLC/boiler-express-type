d4b3221578d315cb2aa47741cbb366d0b3dd6b36
kevinefraim <kevinefraim02@gmail.com>
2022-07-27 11:41:29 -0300

    initial configs

--- dist/app.js
+++ dist/app.js
@@ -2,35 +2,38 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 require("express-async-errors"); //must always be the first, ideal for error handling
-require("reflect-metadata");
-var express_1 = __importDefault(require("express"));
-var morgan_1 = __importDefault(require("morgan"));
-var cors_1 = __importDefault(require("cors"));
-var typeorm_1 = require("typeorm");
-var utils_1 = require("./utils");
-var private_routes_1 = __importDefault(require("./private_routes"));
-var public_routes_1 = __importDefault(require("./public_routes"));
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+const helpers_1 = require("./utils/helpers");
 var PORT = process.env.PORT || '3001';
-var PUBLIC_URL = utils_1.url(PORT);
-var app = express_1["default"]();
-// create a database connection based on the ./ormconfig.js file
-var connectionPromess = typeorm_1.createConnection();
+const PUBLIC_URL = (0, helpers_1.url)(PORT);
+const app = (0, express_1.default)();
+// create a database connection based on the ./ormconfig.ts file
 /*
 Middlewares: every time you see "app.use" we are including a new
 middleware to the express server, you can read more about middle wares here:
 https://developer.okta.com/blog/2018/09/13/build-and-understand-express-middleware-through-examples
 */
-app.use(cors_1["default"]()); //disable CORS validations
-app.use(express_1["default"].json()); // the API will be JSON based for serialization
-app.use(morgan_1["default"]('dev')); //logging
+app.use('/api/v1/stripe/webhook', express_1.default.raw({ type: '*/*' }));
+app.use((0, cors_1.default)()); //disable CORS validations
+app.use((req, res, next) => {
+    if (req.originalUrl === '/api/v1/stripe/webhook') {
+        next();
+    }
+    else {
+        express_1.default.json({ limit: '50mb' })(req, res, next);
+    }
+});
+app.use((0, morgan_1.default)('dev')); //logging
 // render home website with usefull information for boilerplate developers (students)
-app.get('/', function (req, res) { return utils_1.renderIndex(app, PUBLIC_URL).then(function (html) { return res.status(404).send(html); }); });
+app.get('/', (req, res) => (0, helpers_1.renderIndex)(app, PUBLIC_URL).then(html => res.status(404).send(html)));
 // Import public routes from ./src/public_routes.ts file
 // this line has to be ABOVE the JWT middleware to avoid
 // the jwt middleware to influence these enpoints
-app.use(public_routes_1["default"]);
+//app.use(publicRoutes);
 /**
  * ⚠️ IMPORTANT
  * This is the place to include your JWT middleware that will make private routes really private
@@ -39,10 +42,7 @@ app.use(public_routes_1["default"]);
 // Import private routes from ./src/private_routes.ts file
 // this line has to be BELOW the JWT middleware to enforce
 // all these routes to be private
-app.use(private_routes_1["default"]);
 // default empty route for 404
-app.use(function (req, res) { return res.status(404).json({ "message": "Not found" }); });
-// start the express server, listen to requests on PORT
-app.listen(PORT, function () {
-    return console.info("==> \uD83D\uDE0E Listening on port " + PORT + ".\n   Open " + PUBLIC_URL + " in your browser.");
-});
+app.use((req, res) => res.status(404).json({ message: 'Not found' }));
+exports.default = app;
+module.exports = app;
--- dist/entities/Users.js
+++ dist/entities/Users.js
@@ -1,19 +1,4 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -23,37 +8,32 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.Users = void 0;
-var typeorm_1 = require("typeorm");
-var Users = /** @class */ (function (_super) {
-    __extends(Users, _super);
-    function Users() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    __decorate([
-        typeorm_1.PrimaryGeneratedColumn(),
-        __metadata("design:type", Number)
-    ], Users.prototype, "id");
-    __decorate([
-        typeorm_1.Column(),
-        __metadata("design:type", String)
-    ], Users.prototype, "first_name");
-    __decorate([
-        typeorm_1.Column(),
-        __metadata("design:type", String)
-    ], Users.prototype, "last_name");
-    __decorate([
-        typeorm_1.Column({ unique: true }),
-        __metadata("design:type", String)
-    ], Users.prototype, "email");
-    __decorate([
-        typeorm_1.Column(),
-        __metadata("design:type", String)
-    ], Users.prototype, "password");
-    Users = __decorate([
-        typeorm_1.Entity()
-    ], Users);
-    return Users;
-}(typeorm_1.BaseEntity));
+const typeorm_1 = require("typeorm");
+let Users = class Users extends typeorm_1.BaseEntity {
+};
+__decorate([
+    (0, typeorm_1.PrimaryGeneratedColumn)(),
+    __metadata("design:type", Number)
+], Users.prototype, "id", void 0);
+__decorate([
+    (0, typeorm_1.Column)(),
+    __metadata("design:type", String)
+], Users.prototype, "first_name", void 0);
+__decorate([
+    (0, typeorm_1.Column)(),
+    __metadata("design:type", String)
+], Users.prototype, "last_name", void 0);
+__decorate([
+    (0, typeorm_1.Column)({ unique: true }),
+    __metadata("design:type", String)
+], Users.prototype, "email", void 0);
+__decorate([
+    (0, typeorm_1.Column)(),
+    __metadata("design:type", String)
+], Users.prototype, "password", void 0);
+Users = __decorate([
+    (0, typeorm_1.Entity)()
+], Users);
 exports.Users = Users;
