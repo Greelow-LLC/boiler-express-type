8704b0f8adbc39c0457fa10c149233e7f2b42e8a
kevinefraim <kevinefraim02@gmail.com>
2022-07-27 12:35:49 -0300

    added paypal and env.example

---
+++ src/controllers/Paypal.Controller.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { Request, Response } from 'express';
+import { getPaypalToken } from '../utils/paypal';
+
+export const handleOrderState = async (req: Request, res: Response) => {
+  const { token } = req.query;
+  const { purchaseId } = req.params;
+  try {
+    const access_token = await getPaypalToken();
+
+    if (req.params.state === 'success') {
+      const response = await axios.post(
+        `https://api-m.sandbox.paypal.com/v2/checkout/orders/${token}/capture`,
+        {},
+        {
+          headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${access_token}`,
+          },
+        },
+      );
+
+      return res.json('SUCCESS');
+    }
+
+    return res.json('CANCELED');
+  } catch (error: any) {
+    return res.status(error?.status || 400).json({
+      message: error?.message || error?.msg || error,
+      code: error?.codeError || 0,
+      descri: error?.descriError || '',
+    });
+  }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,3 @@
 export { routerErrors } from './Errors.Routes';
 export { routerStripe } from './Stripe.Routes';
+export { routerPaypal } from './Paypal.Routes';
