f4060cc9f5ddead36bf0f80e9e2986fd45c2de1d
nicojoaquin <nicolas.joaquin@greelow.com>
2022-10-03 15:09:21 -0300

    package.json names

---
+++ init.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Initialize a new project from the current branch
+
+# replace $1 with $2 in file $3
+replace() {
+  if [[ $1 != "" && $2 != "" ]]; then
+    perl -pi -e "s/$1/$2/" $3
+  fi
+}
+
+echo "Ready? Let's get your info:"
+
+echo
+echo "Project name?"
+read -r name
+
+echo
+echo "Project description?"
+read -r description
+
+echo
+echo "Initializing project…"
+echo
+
+echo "Cloning boiler..."
+git clone git@github.com:Greelow-LLC/boiler-express-type.git $name
+cd $name
+
+echo "Configuring files…"
+
+replace "greelow-boiler" "$name" "./package.json"
+replace "A reusable boilerplate with Express/Typescript/typeORM for greelow projects", "$description" "./package.json"
+
+cp ./.env.example ./.env
+
+echo
+echo "Initializing Git…"
+rm -rf .git
+git init
+git branch -m develop
+
+cd ..
+
+echo
+echo "🥃 Project Initialized!"
+echo
+echo "To start your project:"
+echo "cd $name"
+echo "npm run db:start"
+echo "npm install"
+echo "npm run dev"
+echo
